391804075adf7ced187f3469b1a3d46e3f0465d2
CristianShux <109182789+CristianShux@users.noreply.github.com>
2024-02-03 01:05:18 -0300

    Lenguaje ensamblador

---
+++ TP ORGANIZACION DEL COMPUTADOR/TPFinal.s
@@ -0,0 +1,626 @@
+.data
+nombre: .asciz " "
+
+artenave:
+.ascii "⢀⣀⣤⣤⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+.ascii "⣿⠉⠀⠀⣩⡿⠋⠛⢛⣷⣶⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+.ascii "⣿⠀⢀⡾⠋⠀⠀⣠⠎⠁⠁⠰⠋⢙⠷⢦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+.ascii "⢿⡴⠋⠀⠀⣠⠞⠁⠀⠀⠀⠀⠀⠉⠀⠰⢋⡿⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+.ascii "⠸⣇⠀⢀⡾⠃⠀⢀⣴⣿⣛⠷⣦⡀⠀⠀⠈⠀⠊⣹⢷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+.ascii "⠀⢿⣴⠋⠀⠀⢀⣿⣽⣿⣿⣷⢻⡇⠀⠀⠀⠀⠀⠰⠋⣻⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+.ascii "⠀⠸⣷⠀⠀⠀⠈⣷⡸⢿⣿⣿⡾⠃⠀⢀⡀⠀⠀⠀⠘⠁⡼⢿⣟⠛⠛⠛⠛⠛⠷⠶⢦⣤⡀⠀⠀⠀⠀⠀\n"
+.ascii "⠀⠀⢹⣶⢂⠀⠀⠈⠛⠻⠟⠋⠁⣴⣿⣿⣿⣳⣄⠀⠀⠈⣰⠋⣻⣷⠒⠶⠶⠶⠶⠶⠦⠬⢽⣦⠀⠀⠀⠀\n"
+.ascii "⠀⠀⠀⢻⣏⡀⠀⠀⠀⠀⠀⠀⢰⡯⣿⣿⣿⡏⣿⠀⠀⠀⢀⡼⠋⠹⣧⠀⠀⠀⠀⠀⠀⠀⠀⢹⡆⠀⠀⠀\n"
+.ascii "⠀⠀⠀⠀⢻⣧⠄⣠⠄⠀⠀⠀⠀⠻⣟⣛⣛⣽⠏⠀⠀⣴⠟⠀⢀⣼⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀\n"
+.ascii "⠀⠀⠀⠀⠀⠹⣯⡥⢂⡀⠀⠀⠀⠀⠈⠉⠉⠀⠀⣠⣾⣁⠀⣠⠞⠉⠐⣿⣄⣀⣀⣀⣀⠀⠀⠀⢸⡇⠀⠀\n"
+.ascii "⠀⠀⠀⠀⠀⠀⠈⢿⣯⠴⢃⡄⠀⠀⠀⠀⠀⢀⡼⠋⢿⡝⠻⢷⣤⣀⣀⣿⠋⠉⠉⠉⠙⢷⣄⠀⠈⣷⠀⠀\n"
+.ascii "⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣯⡴⢃⠀⠀⢀⡴⠋⠀⢀⡼⢻⡄⠀⠈⠙⣿⡁⠀⠀⠀⠀⠀⠀⠙⢷⣄⣿⠀⠀\n"
+.ascii "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⡿⢿⣦⣴⠋⠀⢀⡴⠋⠀⠈⢿⣆⠀⠀⠹⣿⠶⣤⡀⠀⠀⠀⠀⠀⠻⣿⡇⠀\n"
+.ascii "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡇⠸⡏⠙⠻⠶⣯⣾⣗⣀⣠⡾⠋⠻⣷⢶⣿⣷⣮⡙⢷⣄⠀⠀⠀⠀⠈⠁⠀\n"
+.ascii "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⠀⣇⠀⠀⠀⠀⠈⠉⢻⡏⠀⠀⠀⢻⣄⠻⣯⡈⠳⣄⠙⢷⣄⠀⠀⠀⠀⠀\n"
+.ascii "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡄⢻⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠹⣦⡈⠛⢦⣘⢧⡀⠹⣧⡀⠀⠀⠀\n"
+.ascii "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣧⠘⡇⠀⠀⠀⠀⠀⠈⢷⣄⠀⠀⠀⠀⠈⠻⢶⣄⠉⠛⢷⡄⠘⢷⡄⠀⠀\n"
+.ascii "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢧⣷⡀⠀⠀⠀⠀⠀⠀⠙⢷⣄⠀⠀⠀⠀⠀⠙⠻⢦⣄⡀⠀⠈⢿⡄⠀\n"
+.ascii "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠛⠳⠶⢦⣤⣤⣤⣤⣹⣷⣄⠀⠀⠀⠀⠀⠀⠉⠛⠷⣦⣌⣿⡄\n"
+.ascii "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠛\n"
+
+longartenave = .-artenave
+
+presentacion: .ascii "Grupo 10 \n \n ----------Bienvenido a OC Starship----------\n \n Las reglas del juego son: \n 1. Puedes mover la nave con las teclas W, A, S, D \n 2. Debes llegar a la superficie de Argos evitando los asteroides para ganar \n 3. Si golpeas un asteroide o los bordes del mapa, perderás 1 vida \n 4. Tienes 3 vidas disponibles, perdés al acabarse todas \n \n Por favor ingrese su nombre: \n"
+longitudPresentacion = .-presentacion
+
+
+planeta:
+	.ascii " |------------------------------------------------|\n"
+	.ascii " |           Intenta aterrizar en Argos           |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                   +                            |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                       +                        |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " +------------------------------------------------+\n"
+	.ascii " |                                                |\n"
+	.ascii " |               Superficie de Argos              |\n"
+
+longp = .-planeta
+
+ganar:
+	.ascii " |------------------------------------------------|\n"
+	.ascii " |           Intenta aterrizar en Argos           |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |             ¡FELICIDADES, GANASTE!             |\n"
+	.ascii " |                                                |\n"
+	.ascii " |               Gracias por jugar                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |            Pulsa C para reintentar             |\n"
+	.ascii " |              Pulsa Q para salir                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " +------------------------------------------------+\n"
+	.ascii " |                                                |\n"
+	.ascii " |               Superficie de Argos              |\n"
+
+longGanar = .-ganar
+
+over:
+	.ascii " |------------------------------------------------|\n"
+	.ascii " |           Intenta aterrizar en Argos           |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                   GAME OVER                    |\n"
+	.ascii " |                                                |\n"
+	.ascii " |             Pulsa C para reintentar            |\n"
+	.ascii " |               Pulsa Q para salir               |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " |                                                |\n"
+	.ascii " +------------------------------------------------+\n"
+	.ascii " |                                                |\n"
+	.ascii " |               Superficie de Argos              |\n"
+
+longOver = .-over
+
+
+anillo1: .asciz "     *****    ***       ***     ***        **  *"
+longanillo1 = .-anillo1
+anillo2: .asciz "***       ***       **          ******          "
+longanillo2 = .-anillo2
+
+alternador: .word 2
+operador: .word 1
+
+blanco: .asciz " "
+nave: .asciz "X"
+navePosX: .word 26
+navePosY: .word 2
+asteroide: .asciz "*"
+
+saltolinea: .asciz "\n"
+longsaltolinea = .-saltolinea
+
+restantes: .asciz "-------Vidas restantes: "
+longrestantes = .-restantes
+
+vidas: .asciz "3"
+
+cls: .asciz "\x1b[H\x1b[2J"
+lencls = .-cls
+
+textoFinal: .asciz "Presiona C para volver a jugar o Q para salir  \n"
+longtextoFinal = .-textoFinal
+
+teclado: .asciz " "
+
+
+.text
+imprimirString:
+.fnstart
+	mov r7,#4
+	mov r0, #1
+	swi 0
+	bx lr
+.fnend
+
+leerNombre:
+.fnstart
+	mov r1, #0
+	mov r7, #3
+	mov r0, #0
+	mov r2, #8
+	ldr r1, =nombre
+	swi 0
+	bx lr
+.fnend
+
+leerTecla:
+.fnstart
+	mov r1, #0
+	mov r7, #3
+	mov r0, #0
+	mov r2, #1
+	ldr r1, =teclado
+	swi 0
+	bx lr
+.fnend
+
+mostrarAnillo:
+	push {r3}
+	push {r4}
+	push {r8}
+
+	ldr r3, =planeta
+	mov r5, #52
+	mov r6, #18
+	mul r8, r5, r6
+	add r3, r8
+	add r3, #2						/* me posiciono en la ultima fila */
+
+	verificoCaracter:
+		ldrb r4, [r3]
+
+		cmp r4, #0x7c				/* si es un caracter "|" llegué al final de la fila y salgo */
+		bxeq lr
+
+		cmp r4, #0x20				/* si es espacio vacio, verifico patron del anillo y escribo o no segun corresponda */
+		beq verificoAnillo
+
+		add r1, #1
+		add r3, #1
+		b verificoCaracter
+
+	verificoAnillo:
+		ldrb r2, [r1]
+		cmp r2, #0x2a
+		beq escribir
+		add r1, #1
+		add r3, #1
+		b verificoCaracter
+
+	escribir:
+		strb r2, [r3]
+		add r1, #1
+		add r3, #1
+		b verificoCaracter
+
+verificarAsteroide:				/* veo si en la posición actual de la nave hay un asteroide y resto vida */
+	push {lr}
+	push {r6}
+	mov r6, #0x2a
+	mov r1, #0
+	ldrb r1, [r3]
+	cmp r1, r6
+	bleq restarVida
+	cmp r1, r6
+	pop {r6}
+	beq ciclo
+	pop {lr}
+	bx lr
+
+izq:
+	sub r8, #1
+	bx lr
+
+der:
+	add r8, #1
+	bx lr
+
+arr:
+	sub r9, #1
+	sub r8, #1
+	sub r11, #1
+	sub r12, #1
+	bx lr
+abj:
+	add r9, #1
+	add r8, #1
+	add r11, #1
+	add r12, #1
+	bx lr
+
+borrarPosicionAnterior:
+        ldr r4, =blanco
+        ldr r2, [r4]
+        strb r2, [r3]
+        bx lr
+
+
+ganaste:
+	ldr r1, =cls
+	ldr r2, =lencls
+	bl imprimirString
+
+	ldr r1, =ganar
+	ldr r2, =longGanar
+	bl imprimirString
+        b pantallaFinal
+
+gameover:
+	ldr r1, =cls
+	ldr r2, =lencls
+	bl imprimirString
+
+	ldr r1, =over
+	ldr r2, =longOver
+	bl imprimirString
+	b pantallaFinal
+
+
+mostrarNave:
+.fnstart
+	push {lr}
+	ldr r3, =planeta
+	ldr r4, =nave
+	ldr r2, [r4]
+	mov r4, #51
+	mul r5, r4, r9
+	add r3, r5
+	add r3, r8
+	bl verificarAsteroide
+	bl verificarDiamante
+	strb r2, [r3]
+	pop {lr}
+	bx lr
+.fnend
+
+
+verificarDiamante:					/* verifico si la nave pasó por un diamante y sumo vida */
+	push {lr}
+	push {r6}
+	mov r6, #0x202b
+	mov r1, #0
+	ldrh r1, [r3]
+	cmp r1, r6
+	bleq sumarVida
+	pop {r6}
+	pop {lr}
+	bx lr
+
+moverAsteroides:
+	ldr r3, =planeta
+	ldr r2, =longp
+	ldr r4, =blanco
+	ldr r4, [r4]
+	ldr r5, =asteroide
+	ldr r5, [r5]
+
+	buscoAsteroides:
+		cmp r2, #156
+		bxeq lr
+		sub r2, #1
+
+		ldrb r1, [r3]
+		cmp r1, #0x2a
+		beq verificoArriba
+
+		add r3, #1
+		b buscoAsteroides
+
+	verificoArriba:
+		strb r4, [r3]
+		sub r3, #52
+		ldrb r1, [r3]
+
+		cmp r1, #0x20
+		beq mover
+
+		cmp r1, #0x58
+		push {lr}
+		bleq restarVida
+		pop {lr}
+
+		cmp r1, #0x58
+		beq mover
+		add r3, #1
+		b buscoAsteroides
+
+		cmp r1, #0x2d
+		beq eliminarAsteroide
+
+	mover:
+		strb r5, [r3]
+		add r3, #53
+		b buscoAsteroides
+
+	eliminarAsteroide:
+		push {r4}
+		ldr r4, =blanco
+		ldrb r4, [r4]
+		add r3, #52
+		strb r4, [r3]
+		add r3, #1
+		b buscoAsteroides
+
+imprimirAnillo1:
+	push {lr}
+	ldr r1, =anillo1
+	bl mostrarAnillo
+	pop {r8}
+	pop {r4}
+	pop {r3}
+	pop {lr}
+	mov r1, #0
+	ldr r2, =alternador
+	strb r1, [r2]
+	bx lr
+
+
+imprimirAnillo2:
+	push {lr}
+	ldr r1, =anillo2
+	bl mostrarAnillo
+	pop {r8}
+	pop {r4}
+	pop {r3}
+	pop {lr}
+	mov r1, #0
+	ldr r2, =alternador
+	strb r1, [r2]
+	bx lr
+
+omitirAnillo:
+	mov r1, #3
+	ldr r2, =alternador
+	strb r1, [r2]
+
+	ldr r2, =operador
+	ldr r1, [r2]
+
+	cmp r1, #1
+	beq operadorDos
+	bne operadorUno
+
+	operadorUno:
+		mov r1, #1
+		strb r1, [r2]
+		bx lr
+	operadorDos:
+		mov r1, #2
+		strb r1, [r2]
+		bx lr
+
+reestablecerAlternador:
+	ldr r1, =alternador
+	ldr r2, =operador
+	ldr r2, [r2]
+	strb r2, [r1]
+
+mostrarVidas:
+	push {lr}
+	ldr r1, =vidas
+	mov r2, #1
+	bl imprimirString
+	pop {lr}
+	bx lr
+
+sumarVida:						/* sumo una vida cuando atraviesa un + o diamante */
+	add r10, #1
+	push {r1}
+	ldr r1, =vidas
+	strb r10, [r1]
+	pop {r1}		
+	bx lr
+
+restarVida:						/* resto uno a la etiqueta vidas y reinicio coordenadas de la nave */
+.fnstart
+	sub r10, #1
+	push {r1}
+	ldr r1, =vidas
+	strb r10, [r1]
+	pop {r1}
+	ldr r8, =navePosX
+        ldr r8, [r8]						
+        ldr r9, =navePosY					
+        ldr r9, [r9]
+	mov r11, #52
+	mov r12, #3		
+	bx lr
+.fnend
+
+reestablecerTablero:
+	ldr r3, =planeta
+	ldr r2, =longp
+	ldr r4, =blanco
+	ldr r4, [r4]
+	borroAsteroides:
+		cmp r2, #156
+		bxeq lr
+		sub r2, #1
+
+		ldrb r1, [r3]
+		cmp r1, #0x2a
+		beq borrar
+		add r3, #1
+		b borroAsteroides
+	borrar:
+		strb r4, [r3]
+		add r3, #1
+		b borroAsteroides
+
+reestablecerVidas:
+	push {lr}
+	mov r1, #0
+	ldr r1, =vidas
+	mov r2, #0x33
+	strb r2, [r1]
+	bl reestablecerTablero
+	pop {lr}
+	bx lr
+
+
+pantallaFinal:
+	mov r3, #0
+	mov r5, #0
+
+	bl leerTecla
+	ldr r5, [r1]
+
+	cmp r5, #'c'
+	bleq reestablecerVidas
+	beq main
+	cmp r5, #'C'
+	bleq reestablecerVidas
+	beq main
+
+	cmp r5, #'q'
+	beq fin
+	cmp r5, #'Q'
+	beq fin
+
+	b pantallaFinal
+
+
+.global main
+main:
+	ldr r1, =artenave
+	ldr r2, =longartenave
+	bl imprimirString
+
+
+	ldr r1, =presentacion
+	ldr r2, =longitudPresentacion
+	bl imprimirString		
+
+	bl leerNombre			
+	ldr r6, [r1]
+	mov r1, #0
+
+	ldr r8, =navePosX
+	ldr r8, [r8]			
+	ldr r9, =navePosY
+	ldr r9, [r9]			 
+
+	ldr r10, =vidas
+    ldrb  r10, [r10]			
+
+	mov r11, #52				/* limites del tablero */
+	mov r12, #3
+
+
+ciclo:
+	cmp r10, #0x2F				/* verifico las vidas restantes, si son 0 salto a gameover */
+	bleq gameover
+
+	cmp r8, r11					/* verifico si la nave choca con el borde derecho y salto para restar una vida */
+	bleq restarVida
+
+	cmp r8, r12                 /* verifico si la nave choca con el borde izquierdo y salto para restar una vida */
+        bleq restarVida
+
+	cmp r9, #19					/* si llega a la ultima fila, salto a la pantalla ganaste */
+	beq ganaste
+
+	mov r1, #0
+	mov r2, #0
+	ldr r1, =cls				/* caso contrario limpio pantalla */
+	ldr r2, =lencls
+	bl imprimirString
+
+	bl mostrarNave
+
+	ldr r2, =alternador
+	ldrb r2, [r2]
+
+	cmp r2, #0x0
+	bleq omitirAnillo
+
+	cmp r2, #0x1
+	bleq imprimirAnillo2
+
+	cmp r2, #0x2
+	bleq imprimirAnillo1
+
+	cmp r2, #0x3
+	bleq reestablecerAlternador			/* muestro anillo, modifico el alternador para próxima iteración */
+
+	mov r1, #0
+	mov r2, #0
+	ldr r1, =planeta
+	ldr r2, =longp
+	bl imprimirString					/* imprimo el tablero */
+
+
+	mov r1, #0
+	mov r2, #0
+	ldr r1, =restantes
+	ldr r2, =longrestantes
+	bl imprimirString
+
+	bl mostrarVidas
+
+	mov r1, #0
+	mov r2, #0
+	ldr r1, =saltolinea
+	ldr r2, =longsaltolinea
+	bl imprimirString
+
+
+	mov r5, #0
+	mov r4, #0
+	push {r3}
+	bl moverAsteroides
+	pop {r3}
+
+
+	bl leerTecla				
+	ldr r5, [r1]
+	mov r1, #0
+
+
+        cmp r5,#'a'    			 /*evaluacion de teclas de movimiento*/
+        bleq izq
+        cmp r5,#'A'
+        bleq izq
+
+        cmp r5,#'w'
+        bleq arr
+        cmp r5,#'W'
+        bleq arr
+
+        cmp r5,#'d'
+        bleq der
+        cmp r5,#'D'
+        bleq der
+
+        cmp r5,#'s'
+        bleq abj
+        cmp r5,#'S'
+        bleq abj
+
+	bl borrarPosicionAnterior
+
+
+	b ciclo				/* repito */
+
+
+fin:
+	mov r7, #1
+	swi 0
+
